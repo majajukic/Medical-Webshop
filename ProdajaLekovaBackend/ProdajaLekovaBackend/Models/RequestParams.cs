88972fbb72fd67f95c43b6b42710baba7e447cbe
Maja Jukic <mjukic2000@gmail.com>
2023-04-18 00:22:13 +0200

    Izmene za paginaciju

--- ProdajaLekovaBackend/ProdajaLekovaBackend/Models/RequestParams.cs
+++ ProdajaLekovaBackend/ProdajaLekovaBackend/Models/RequestParams.cs
@@ -5,7 +5,7 @@
         const int maxPageSize = 12;
         public int PageNumber { get; set; } = 1;//default page number
 
-        private int _pageSize = 8;//default page size
+        private int _pageSize = 9;//default page size
 
         public int PageSize
         {
